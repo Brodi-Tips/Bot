89968e92d15797344b2bfc821ccfc8f7316bd67b
Julio Lima <julio_flima@hotmail.com>
2020-11-08 16:50:17 -0300

    Reorder Games.

--- src/app/services/Games.js
+++ src/app/services/Games.js
@@ -1,18 +1,18 @@
 module.exports = class Games {
   set(newGames = [], oldGames = []) {
     if (newGames.length && oldGames.length) {
-      const remove = this.games.filter((each) => !newGames.includes(each));
-
       const add = newGames.filter((each) => !oldGames.includes(each));
 
       const union = [...new Set([...oldGames, ...newGames])];
 
       const keep = union.filter((each) => newGames.includes(each) && oldGames.includes(each));
 
+      const remove = this.games.filter((each) => !newGames.includes(each));
+
       this.games = { keep, add, remove };
     }
-    if (!oldGames.length && newGames.length) this.games = { keep: [], add: newGames };
-    if (!oldGames.length && !newGames.length) this.games = { keep: [], add: [] };
+    if (!oldGames.length && newGames.length) this.games = { add: newGames, keep: [], remove: [] };
+    if (!oldGames.length && !newGames.length) this.games = { add: [], keep: [], remove: [] };
   }
 
   get() {
