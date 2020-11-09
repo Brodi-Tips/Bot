b4dc5b3c9f96a721d6de7092439d62b80a9c97be
Julio Lima <julio_flima@hotmail.com>
2020-11-08 21:19:04 -0300

    Add scripts.

---
+++ scripts/openCloseChromeHidden.sh
@@ -0,0 +1,11 @@
+## Init Loop
+echo "Init Loop!";
+
+while :; do
+    echo "Opening Chrome!";
+    xvfb-run --server-args='-screen 1, 1024x768x16' google-chrome-stable --no-sandbox --new-window --start-maximized https://www.bet365.com/#/AS/B83/ > /dev/null & sleep 30; 
+    echo "Closing Chrome!";
+    kill -9 $(pgrep -d' ' -f xvfb) & sleep 30; 
+done
+
+
