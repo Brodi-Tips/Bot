32a93e7bafdfc40aee5b674dc5e59f9defd67689
Julio Lima <julio_flima@hotmail.com>
2020-11-21 20:36:45 -0300

    Change Google Chrome to Stable version.

--- scripts/openCloseChromeHidden.sh
+++ scripts/openCloseChromeHidden.sh
@@ -3,7 +3,7 @@ echo "Init Loop!"
 
 while :; do
     echo "Opening Chrome!"
-    xvfb-run --server-args='-screen 0, 1024x768x16' google-chrome --no-sandbox --new-window --start-maximized 'https://www.bet365.com/#/AS/B83/' >/dev/null &
+    xvfb-run --server-args='-screen 0, 1024x768x16' google-chrome-stable --no-sandbox --new-window --start-maximized 'https://www.bet365.com/#/AS/B83/' >/dev/null &
     sleep 30
     echo "Closing Chrome!"
     kill -9 $(pgrep -d' ' -f xvfb) &
