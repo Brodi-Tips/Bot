506a3f27b32adfb917f01c725643925e2c9bbef7
Julio Lima <julio_flima@hotmail.com>
2020-12-28 20:39:04 -0300

    Update last commits.

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -10,13 +10,14 @@ echo "Building image..."
 sudo docker build -f ./Dockerfile -t futsal-bot .
 
 echo "Running image..."
-sudo docker run -t -d --name futsal futsal-bot
+sudo docker run -t --name futsal futsal-bot
 
+echo "Opening chrome..."
 # sudo docker exec -d futsal bash /usr/src/app/scripts/openCloseChromeHidden.sh
 
 # Execute and show logs
-# sudo docker run -t -d futsal-bot
+# sudo docker run -t futsal-bot
 
 # Enter execute and enter.
-# sudo docker run -t -d --name futsal futsal-bot
+sudo docker run -t -d --name futsal futsal-bot
 # sudo docker exec -it futsal /bin/bash
