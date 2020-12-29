95b846a7159a6e8ec3a4c7090f07ed86ee71103b
Julio Lima <julio_flima@hotmail.com>
2020-12-28 22:55:51 -0300

    Comment script.

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# Cleaning terminal.
+echo "Cleaning terminal."
 clear
-echo "Stoping all containers..."
+
+echo "Stoping all containers."
 sudo docker stop $(docker ps -a -q)
 
-echo "Removing all containers..."
+echo "Removing all containers."
 sudo docker rm $(docker ps -a -q)
 
 echo "Building image..."
@@ -12,15 +16,18 @@ sudo docker build -f ./Dockerfile -t futsal-bot .
 echo "Running image with NoVNC."
 sudo docker run -d -p 3000:3333 -p 26901:6901 --name futsal -t futsal-bot
 
+echo "Executing scripts inside docker to open scraping in chrome."
+sudo docker exec -d -it futsal bash /usr/src/app/scripts/openCloseChrome.sh
+# sudo docker exec -d -it futsal bash /usr/src/app/scripts/openCloseChromeHidden.sh
+
+echo "Executing scripts inside docker to local Node Server."
+sudo docker exec -d -it futsal node /usr/src/app/src/server.js
+
 echo "OS Server"
 echo "Host NoVNC: http://172.17.0.2:6901/vnc_lite.html"
 echo "Password: headless"
-echo ""
 
-# Enter execute.
-sudo docker exec -d -it futsal bash /usr/src/app/scripts/openCloseChrome.sh
-# sudo docker exec -d -it futsal bash /usr/src/app/scripts/openCloseChromeHidden.sh
-sudo docker exec -d -it futsal node /usr/src/app/src/server.js
+echo ""
 
 echo "Node Server"
 echo "Host: http://172.17.0.2:3000"
